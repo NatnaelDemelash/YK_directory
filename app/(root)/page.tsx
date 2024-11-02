3ee27d5d0ba6cce63d236ef3df33c3c0351c0ffd
NatnaelDemelash <natidemelash074@gmail.com>
2024-11-02 23:05:32 +0300

    setup font and theme

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h2 className="text-2xl">Home Page</h2>;
+  return (
+    <section className="pink_container">
+      <h2 className="heading">
+        Pitch Your Startup, <br /> Connect with Entrepreneurs
+      </h2>
+      <p className="sub-heading !max-w-3xl">
+        We are a platform for entrepreneurs to pitch their ideas and connect
+        with other like-minded individuals.
+      </p>
+    </section>
+  );
 }
